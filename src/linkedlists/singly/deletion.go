90d3875f80aec60d2bf2ad5c9625a6d58b39e874
Zaw <zawlinnnaing0018@gmail.com>
2023-06-03 14:07:14 +0700

    feat: added delete middle singly linked list

--- src/linkedlists/singly/deletion.go
+++ src/linkedlists/singly/deletion.go
@@ -8,11 +8,33 @@ func DeleteHead(head *SinglyLinkedListNode) *SinglyLinkedListNode {
 	return temp
 }
 
-func DeleteMiddle(head *SinglyLinkedListNode) {}
+func DeleteMiddle(head *SinglyLinkedListNode, position int) *SinglyLinkedListNode {
+	prev := head
+	current := head
+	for i := 0; i <= position; i += 1 {
+		if i == position && current != nil {
+			prev.Next = current.Next
+			current.Next = nil
+			break
+		}
+		prev = current
+		if prev == nil {
+			break
+		}
+		current = current.Next
+
+	}
+	return head
+
+}
 
 func DeleteExample() {
 	deleteHeadNodes := PrepareExample()
 	remainingNodes := DeleteHead(&deleteHeadNodes[0])
 	fmt.Println("Running delete head example:")
 	PrintNode(remainingNodes)
+	deleteMiddleNodes := PrepareExample()
+	fmt.Println("Running delete middle example:")
+	DeleteMiddle(&deleteMiddleNodes[0], 1)
+	PrintNode(&deleteMiddleNodes[0])
 }
