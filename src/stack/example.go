65e3963a43ed16589fd9756623bc7ab4ce8f55d6
Zaw <zawlinnnaing0018@gmail.com>
2023-06-10 15:31:26 +0700

    feat: added monotonic stack

--- src/stack/example.go
+++ src/stack/example.go
@@ -32,10 +32,31 @@ func runSpaceOptimizedExample() {
 	fmt.Printf("Min value for space optimized stack: %d \n", spaceOptimizedStack.GetMin())
 }
 
+func runMonotonicStackExample(mode MonotonicStackMode) {
+	var initialElements []int
+	if mode == Increasing {
+		initialElements = []int{1, 3, 4, 6}
+	} else {
+		initialElements = []int{6, 4, 3, 1}
+	}
+	monotonicStack := CreateMonotonicStack(mode, initialElements)
+	monotonicStack.innerStack.PrintStack("")
+	elementsToPush := []int{2, 4, 3}
+	for i := 0; i < len(elementsToPush); i++ {
+		fmt.Printf("Monotonic stack after pushing %d: \n", elementsToPush[i])
+		monotonicStack.Push(elementsToPush[i])
+		monotonicStack.innerStack.PrintStack("")
+	}
+}
+
 func RunAllStackExamples() {
 	fmt.Println("Running stack examples --------")
 	fmt.Println("Running special stack examples")
 	runSpecialStackExample()
 	fmt.Println("Running space optimized stack examples")
 	runSpaceOptimizedExample()
+	fmt.Println("Running monotonic increasing stack example")
+	runMonotonicStackExample(Increasing)
+	fmt.Println("Running monotonic decreasing stack example")
+	runMonotonicStackExample(Decreasing)
 }
---
+++ src/stack/monotonic.go
@@ -0,0 +1,40 @@
+package stack
+
+type MonotonicStackMode int
+
+const (
+	Increasing MonotonicStackMode = iota
+	Decreasing
+)
+
+type MonotonicStack struct {
+	innerStack *Stack[int]
+	Mode       MonotonicStackMode
+}
+
+func (stack *MonotonicStack) Push(element int) {
+	for !stack.innerStack.IsEmpty() {
+		elementFromStack := stack.innerStack.Peak()
+		if stack.shouldPopElement(elementFromStack, element) {
+			stack.innerStack.Pop()
+		} else {
+
+			break
+		}
+	}
+	stack.innerStack.Push(element)
+}
+
+func (stack *MonotonicStack) shouldPopElement(elementFromStack int, incomingElement int) bool {
+	if stack.Mode == Increasing {
+		return elementFromStack >= incomingElement
+	}
+	return elementFromStack <= incomingElement
+}
+
+func CreateMonotonicStack(mode MonotonicStackMode, initialElements []int) MonotonicStack {
+	return MonotonicStack{
+		innerStack: CreateStack(initialElements),
+		Mode:       mode,
+	}
+}
